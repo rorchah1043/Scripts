245eb33c0fccd53727402675129fcc88ce3934a6
rorchah1043 <romankiy@mail.ru>
2021-09-21 13:02:02 +0300

    1

---
+++ PickUpWeapon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUpWeapon : MonoBehaviour
+{
+    private Transform weaponPosition;
+    [SerializeField] private Canvas canvas;
+    // Start is called before the first frame update
+    private void Start()
+    {        
+        weaponPosition = GetComponent<Transform>();
+        if(canvas == null)
+        {
+            Debug.Log("Назначьте CANVAS!!!");
+        }
+        canvas.enabled = !canvas.enabled;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        canvas.enabled = true;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (other.CompareTag("Weapon"))
+            {
+                other.enabled = false;
+                other.transform.SetParent(weaponPosition);
+                other.transform.position =  weaponPosition.position;
+                if (other.transform.rotation.eulerAngles.y != 0 || weaponPosition.GetComponentInParent<Transform>().rotation.eulerAngles.y != 0)
+                {
+                    weaponPosition.transform.Rotate(0, 180, 0);
+                }
+            }
+            canvas.enabled = false;
+        }
+    }
+}
---
+++ PlayerController (1).cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Canvas canvas;
+    private Animator triggerAnimator;
+    private AnimatorControllerParameter objectAnimatorTrigger;
+    [SerializeField] private int triggerParameterIndex;
+    [SerializeField] private string button;
+
+    private void Start()
+    {
+        canvas.enabled = false;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerDirection = new Vector3(hor, 0, ver) * 2 * Time.deltaTime;
+        transform.Translate(playerDirection, Space.Self);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<InteractiveController>())
+        {
+            canvas.enabled = true;  
+        }
+
+        if (triggerAnimator == null)
+        {
+            triggerAnimator = other.GetComponent<Animator>();
+            objectAnimatorTrigger = triggerAnimator.GetParameter(0);
+        }
+        DoTriggeredAnimation();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(button))
+        {
+            triggerAnimator.SetTrigger(objectAnimatorTrigger.name);
+            Debug.Log("Анимация сработала");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<InteractiveController>())
+        {
+            canvas.enabled = false;
+        }
+    }
+
+    public void DoTriggeredAnimation()
+    {
+        Debug.Log("Объект-триггер что-то делает");
+    }
+}
